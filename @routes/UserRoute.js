6bfa8d853917d456d1f9c8bea518fce208055d33
arty <sverrevanhoepen12@outlook.com>
2022-09-18 15:01:38 +0200

    invite system

--- @routes/UserRoute.js
+++ @routes/UserRoute.js
@@ -10,35 +10,53 @@ router.post("/register", async (req,res) => {
     const username = req.body.username.replace(/[^a-zA-Z0-9]/g, "");
     const password = req.body.password.replace(/[^a-zA-Z0-9]/g, "");
 
-
-    // Check if the user already exists
-    let sql = 'SELECT * FROM users WHERE username = ?';
-    let data = [username];
-    const user = db.query(sql, data, (err, result) => {
+    // Check if the invite code is valid
+    let sql = 'SELECT * FROM invites WHERE code = ?';
+    let data = [req.body.invite];
+    const result = db.query(sql, data, (err, result) => {
         if (err) throw err;
-        if (result.length > 0) {
-            return res.status(400).json({error: "User already exists"});
-        } else {
-            sql = 'SELECT * FROM users WHERE email = ?';
-            data = [req.body.email];
-            const email = db.query(sql, data, (err, result) => {
-                if (err) throw err;
-                if (result.length > 0) {
-                    return res.status(400).json({error: "Email already in use"});
-                } else {
+        if (result.length == 0) return res.status(400).json({error: "Invalid invite code"});
+        if (result[0].used == true) return res.status(400).json({error: "Invite code already used"});
+
+        // Check if the user already exists
+        sql = 'SELECT * FROM users WHERE username = ?';
+        data = [username];
+        const user = db.query(sql, data, (err, result) => {
+            if (err) throw err;
+            if (result.length > 0) {
+                return res.status(400).json({error: "User already exists"});
+            } else {
+                sql = 'SELECT * FROM users WHERE email = ?';
+                data = [req.body.email];
+                const email = db.query(sql, data, (err, result) => {
+                    if (err) throw err;
+                    if (result.length > 0) {
+                        return res.status(400).json({error: "Email already in use"});
+                    } else {
                         // Create a new user
                         const simpleCrypto = new SimpleCrypto(password);
                         const encryptedPassword = simpleCrypto.encrypt(password);
                         sql = 'INSERT INTO users (email, username, password) VALUES (?, ?, ?)';
                         data = [req.body.email, username, encryptedPassword];
-                        const result = db.query(sql, data, (err, result) => {
+                        const result23 = db.query(sql, data, (err, result) => {
                             if (err) throw err;
-                            req.session.code = "Successfully registered";
-                            return res.redirect("/login");
+                            sql = 'SELECT * FROM users WHERE username = ?';
+                            data = [username];
+                            db.query(sql, data, (err, result) => {
+                                if (err) throw err;
+                                sql = 'UPDATE invites SET used = ?, usedBy = ? WHERE code = ?';
+                                data = [true, result[0].id, req.body.invite];
+                                const result2 = db.query(sql, data, (err, result) => {
+                                    if (err) throw err;
+                                    req.session.code = "Successfully registered";
+                                    return res.redirect("/login");
+                                });
+                            });
                         });
-                }
-            });
-        }
+                    }
+                });
+            }
+        });
     });
 })
 
--- d.sql
+++ d.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Users(id int AUTO_INCREMENT, email VARCHAR(255), username VARCHAR(255), password VARCHAR(255), PRIMARY KEY(id));
-CREATE TABLE Invites(code VARCHAR(255), createdBy INT, used BOOL, PRIMARY KEY(code), FOREIGN KEY(createdBy) REFERENCES Users(id));
+CREATE TABLE Invites(code VARCHAR(255), createdBy INT, used BOOL, usedBy INT, PRIMARY KEY(code), FOREIGN KEY(createdBy) REFERENCES Users(id), FOREIGN KEY(usedBy) REFERENCES Users(id));
 
 
 ALTER TABLE Users AUTO_INCREMENT = 1
